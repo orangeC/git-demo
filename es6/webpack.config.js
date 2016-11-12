4f6c7ee7d9b369239b0754ec613a13ae64aed593
orangeC <18332563755@163.com>
2016-11-12 15:21:16 +0800

    debug

--- es6/webpack.config.js
+++ es6/webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: path.resolve(__dirname, 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'build'),
+        // path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
 };
