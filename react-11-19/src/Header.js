8de6c6bb44a03ad77af7219a5eb7e159618c6241
orangeC <18332563755@163.com>
2016-11-19 11:00:05 +0800

    style

--- react-11-19/src/Header.js
+++ react-11-19/src/Header.js
@@ -2,9 +2,34 @@ import React from "react"
 
 
 class Header extends React.Component{
+	styles(){
+		return {
+			color:"blue"
+		}
+	}
 	render(){
+		let styles = {
+			ul:{
+				listStyle:"none",
+				padding:"0"
+			},
+			li:{
+				display:"inline-block",
+				padding:"10px 20px"
+			}
+		}
+		console.log(this.styles())
 		return (
-				<div>header</div>
+				<div>
+					<h1 style={this.styles()}>get Bootstrap</h1>
+					<ul style={styles.ul}>
+						<li style={styles.li}>Bootstrap</li>
+						<li style={styles.li}>get start</li>
+						<li style={styles.li}>CSS</li>
+						<li style={styles.li}>Components</li>
+						<li style={styles.li}>Javascript</li>
+					</ul>
+				</div>
 			)
 	}
 }
